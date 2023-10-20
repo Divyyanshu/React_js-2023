e01da028ca44fee2e25c498cc1745276b5a4611c
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-10-20 21:49:11 +0530

    day2react.js

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import MyHeading from "./components/MyHeading";
+
+
+function App() {
+  return (
+    <Fragment>
+        <MyHeading/>
+    <div><h1>jai shree ram </h1>
+      <p>huiffhishfih</p></div>
+      <div>
+      <MyHeading/>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/MyHeading.jsx
@@ -0,0 +1,6 @@
+const MyHeading = () =>
+
+<h1>kya kreogye raect sikh kr ap</h1>
+
+export default MyHeading;
+
