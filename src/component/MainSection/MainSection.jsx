27ebebea057cfb5ca24b898c962f1f65a2c1f7fc
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-11-23 14:26:24 +0530

    add google image

--- src/component/MainSection/MainSection.jsx
+++ src/component/MainSection/MainSection.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import Google from '../../img/ggimg.png'
 import './MainSection.css'
+import Google from '../../img/ggimg.png'
 
 const MainSection = () => {
   return (
     <div className="main-section">
       <img src={Google} alt="" />
-    </div>
+      {/* search bar design */}
+      <div className="search-bar">
+          <div className="search-left">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <div className="search-right">
+          <i class="fa-solid fa-microphone"></i>
+          <i class="fa-solid fa-square-virus"></i>
+          </div>
+        </div>
+        {/* buttons */}
+        <div className="m-buttons">
+          <button>Google Search</button>
+          <button>I'm Feeling Lucky</button>
+        </div>
+      </div>
   )
 }
 
